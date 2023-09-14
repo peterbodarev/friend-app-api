c158c436e41af2f3785b1b99d7947ad07df812ba
Petru Bodarev <peter.bodarev.dev@gmail.com>
2023-09-14 16:50:51 +0300

    Update allow origin

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(
     origin: "*",
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
