e09f3101c040ba3e388f5e572f576c9d06f670f8
Petru Bodarev <peter.bodarev.dev@gmail.com>
2023-09-14 16:43:21 +0300

    Add cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
